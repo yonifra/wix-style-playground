48da76be43c74b53fc4db8daa397d2ae8682edb5
Jonathan Fraimorice <yonifra@gmail.com>
2019-06-26 12:19:47 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Import all relevant components from this file
+import { Modal, MessageBoxFunctionalLayout } from "./wix-style-react";
+
+const App = () => (
+  <Modal
+    isOpen={true}
+    onRequestClose={() => console.log("")}
+    contentLabel="Modal Example"
+    scrollableContent={false}
+  >
+    <MessageBoxFunctionalLayout
+      theme="blue"
+      title="Lorem Ipsum!"
+      confirmText="OK"
+      cancelText="Cancel"
+      onOk={() => console.log("")}
+      onCancel={() => console.log("")}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum
+    </MessageBoxFunctionalLayout>
+  </Modal>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
---
+++ src/wix-style-react.js
@@ -0,0 +1,1 @@
+module.exports = window["wix-style-react"];
